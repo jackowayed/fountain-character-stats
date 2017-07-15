1c9e736adb62cf3cabc39e63993eada6c92927b1
Daniel Jackoway <commits@danieljackoway.com>
2017-07-15 16:04:19 -0400

    It works

---
+++ stats.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import collections
+import os
+import re
+import sys
+
+Character = collections.namedtuple("Character", ["lines", "words"])
+
+# State machine
+COULD_SEE_CHARACTER = 1
+JUST_SAW_CHARACTER = 2
+IN_DIALOGUE = 3
+CANT_SEE_CHARACTER = 4
+
+def is_character_name(line):
+    if line and line[0] == "@":
+        return True
+    # Once you strip whitespace, a character is:
+    # 1 or more ALLCAPS words, optionally followed by
+    # a parenthetical that can have basically whateer in it.
+    # TODO support the parentheticals (\([\w\./-]+\))?
+    return re.search(r"^[A-Z]+( [A-Z]+)*$", line)
+
+def is_dialogue_line(line):
+    return line.strip() or len(line) > 2
+
+def count_words(line):
+    return len(line.split())
+
+
+def parse(filename):
+    characters = collections.defaultdict(lambda: Character(0, 0))
+    state = IN_DIALOGUE
+    current_character = None
+    with open(filename) as f:
+        for line in f:
+            cleaned = line.strip()
+            if state == COULD_SEE_CHARACTER:
+                if is_character_name(cleaned):
+                    current_character = cleaned[1:] if cleaned[0] == "@" else cleaned
+                    state = JUST_SAW_CHARACTER
+            elif state == JUST_SAW_CHARACTER:
+                if is_dialogue_line(line):
+                    c = characters[current_character]
+                    characters[current_character] = Character(c.lines + 1, c.words + count_words(cleaned))
+                    state = IN_DIALOGUE
+                elif not cleaned:
+                    # The line is blank, so the next line could be a character name
+                    state = COULD_SEE_CHARACTER
+                else:
+                    # The line isn't blank, so the next line can't be a character name
+                    state = CANT_SEE_CHARACTER
+            elif state == IN_DIALOGUE:
+                if is_dialogue_line(line):
+                    c = characters[current_character]
+                    characters[current_character] = Character(c.lines, c.words + count_words(cleaned))
+                else:
+                    assert not cleaned
+                    state = COULD_SEE_CHARACTER
+            elif state == CANT_SEE_CHARACTER:
+                if not cleaned:
+                    state = COULD_SEE_CHARACTER
+    return characters
+
+
+if __name__ == '__main__':
+    for charname, stats in sorted(parse(sys.argv[1]).iteritems(), key=lambda kv: kv[1].words, reverse=True):
+        print "%s\t%s\t%s" % (charname, stats.words, stats.lines)
+                    
---
+++ stats_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from stats import *
+import unittest
+
+class TestUtilityFunctions(unittest.TestCase):
+    def test_is_character_name(self):
+        self.assertTrue(is_character_name("JOHN"))
+        self.assertTrue(is_character_name("JOHN SMITH"))
+        # TODO reenable
+        #self.assertTrue(is_character_name("JOHN SMITH (Hello 1.)"))
+
+        self.assertFalse(is_character_name("John"))
+        self.assertFalse(is_character_name("JOHN Smith"))
+        self.assertFalse(is_character_name("JOHN   SMITH"))
+
+if __name__ == '__main__':
+    unittest.main()
